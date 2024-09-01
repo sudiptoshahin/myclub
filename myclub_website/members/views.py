3d323691b6fc9bb103c5609761f4f422f6b31f38
sudiptoshahin <sudipto.shine@gmail.com>
2024-09-01 09:31:55 +0600

    login done

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -9,6 +9,7 @@ def login_user(request: HttpRequest):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             # Redirect to a success page.
